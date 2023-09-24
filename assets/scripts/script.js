6f4381a74ff42347b98b490f073e97a801f34f95
ewi josepha <ewijosepha@gmail.com>
2023-09-24 11:31:06 +0200

    modifying functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,37 +2,42 @@ const donebtn = document.getElementById('donebtn')
 const revealAns = document.getElementById('reveal-ans')
 
 
-function scaleBalance () {
-let firstnumber = document.getElementById('firstnumber').value
-const secondnumber = document.getElementById('secondnumber').value
-const arr = document.getElementById('array').value
+function scaleBalance() {
+  let firstnumber = document.getElementById('firstnumber').value
+  const secondnumber = document.getElementById('secondnumber').value
+  const arr = document.getElementById('array').value
   const splitFirstNumber = firstnumber.split()
   const splitSecondNumber = secondnumber.split()
   console.log(splitFirstNumber)
-   splitarr = arr.split('')
+  splitarr = arr.split('')
   //  let arr2 =splitarr.length
-  //  console.log(splitarr);
+  console.log(splitarr);
   //  console.log(arr2);
-   
+  for (let i = 0; i < splitarr.length; i++) {
+    for (let j = 0; j < splitarr.length; j++) {
 
-   for(i=0; i<splitarr.length; i++){
-    for(j=0; j<splitarr.length;j++){ 
-      console.log(splitarr.length[0]);
+      console.log(splitarr[j]);
+
+      console.log(splitFirstNumber)
+
+
       // revealAns.innerHTML = arr2
-      if(splitFirstNumber + splitarr[i] === splitSecondNumber + splitarr[j]){
+      if (splitFirstNumber + splitarr[i] === splitSecondNumber + splitarr[j]) {
+
+
         return splitarr[i], splitarr[j]
-      }else{
-        console.log('imbalanced')
-        revealAns.innerHTML = "imbalannced"
+      } else {
+        revealAns.innerHTML = "imbalanced"
+        return
       }
-   }
-   }
-revealAns.innerHTML
+    }
+  }
+  revealAns.innerHTML
 
 
 }
 
-donebtn.addEventListener('click', ()=>{
+donebtn.addEventListener('click', () => {
   scaleBalance()
 
 })
