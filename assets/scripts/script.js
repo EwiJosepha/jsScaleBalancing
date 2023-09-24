299eec08f4188447269aab2635c6fc839a49305f
ewi josepha <ewijosepha@gmail.com>
2023-09-24 16:44:58 +0200

    adjusting functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,17 @@
 const donebtn = document.getElementById('donebtn')
 const revealAns = document.getElementById('reveal-ans')
 
-
-function scaleBalance() {
-  let firstnumber = document.getElementById('firstnumber').value
+function scaleBalance () {
+  const firstnumber = document.getElementById('firstnumber').value
   const secondnumber = document.getElementById('secondnumber').value
   const arr = document.getElementById('array').value
-  let splitFirstNumber = firstnumber.split()
+  const splitFirstNumber = firstnumber.split()
   const splitSecondNumber = secondnumber.split()
   console.log(splitFirstNumber)
   console.log(splitSecondNumber)
-  splitarr = arr.split('')
+  const splitarr = arr.split('')
   //  let arr2 =splitarr.length
-  console.log(splitarr);
+  console.log(splitarr)
   //  console.log(arr2);
   for (let i = 0; i < splitarr.length; i++) {
     for (let j = 0; j < splitarr.length; j++) {
@@ -22,15 +21,14 @@ function scaleBalance() {
       if (+splitFirstNumber + +splitarr[i] === +splitSecondNumber + +splitarr[j]) {
         console.log(`${splitarr[i]} ${splitarr[j]}`)
         revealAns.innerHTML = `${splitarr[i]} ${splitarr[j]}`
-        return;
+        return
       }
     }
   }
-  revealAns.innerHTML = "imposiible"
-  return
+  revealAns.innerHTML = 'imposiible'
+  
 }
 
 donebtn.addEventListener('click', () => {
   scaleBalance()
-
 })
